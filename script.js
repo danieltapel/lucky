ce262b02de2c27673a2dcaaab7d9f862e7f249f9
danieltapel <73966070+danieltapel@users.noreply.github.com>
2022-10-10 21:19:50 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+//get "lucky" in java(nese): "begja"
+function makeid(length) { //i invented plagiarism
+  var result = '';
+  var characters = 'abcdefghijklmnopqrstuvwxyz';
+  var charactersLength = characters.length;
+  for (var i=0;i<length;i++) {
+    result += characters.charAt(Math.floor(Math.random()* charactersLength));
+  }
+  return result; 
+}
+
+let luck = document.cookie.slice(-5);
+
+if (luck == "lucky") {
+  document.getElementById("possibleFlag").innerHTML = 'try using java<i class="hidethis">nese</i>';
+  document.getElementById("tabText").innerHTML = "lucky?";
+} else if (luck != "begja") {
+    document.getElementById("possibleFlag").innerHTML = "UDCTF{i_question_the_accuracy_of_google_translate}"; 
+  document.getElementById("tabText").innerHTML = "lucky!";
+} else {
+
+  document.cookie = "lucky=" + makeid(5);
+}
